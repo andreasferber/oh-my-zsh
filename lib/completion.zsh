671adea5da27b9ee48cba6416a2a5aa5b8066720
Andreas Ferber <af+git@chaos-agency.de>
2013-09-24 21:44:54 +0200

    Improve SSH known_hosts parsing.

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -40,6 +40,22 @@ fi
 # disable named-directories autocompletion
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
+# use /etc/hosts and known_hosts for hostname completion
+[ -r /etc/ssh/ssh_known_hosts ] && _global_ssh_hosts=(${${(ps:,:)${(pj:,:)${${${(f)"$(</etc/ssh/ssh_known_hosts)"}%%\#*}%%\ *}}}:#[0-9]*}) || _global_ssh_hosts=()
+[ -r ~/.ssh/known_hosts ] && _ssh_hosts=(${${(ps:,:)${(pj:,:)${${${(f)"$(<$HOME/.ssh/known_hosts)"}%%\#*}%%\ *}}}:#[0-9]*}) || _ssh_hosts=()
+[ -r ~/.ssh/config ] && _ssh_config=($(cat ~/.ssh/config | sed -ne 's/Host[=\t ]//p')) || _ssh_config=()
+[ -r /etc/hosts ] && : ${(A)_etc_hosts:=${(s: :)${(ps:\t:)${${(f)~~"$(</etc/hosts)"}%%\#*}##[:blank:]#[^[:blank:]]#}}} || _etc_hosts=()
+hosts=(
+  "$_ssh_config[@]"
+  "$_global_ssh_hosts[@]"
+  "$_ssh_hosts[@]"
+  "$_etc_hosts[@]"
+  "$HOST"
+  localhost
+)
+zstyle ':completion:*:hosts' hosts $hosts
+zstyle ':completion:*' users off
+
 # Use caching so that commands like apt and dpkg complete are useable
 zstyle ':completion::complete:*' use-cache 1
 zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
